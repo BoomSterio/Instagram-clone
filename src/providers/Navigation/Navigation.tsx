dd372bd0920a4b7fb9691664f2ccb2e707cc33c5
Maksym <max.sharinov@gmail.com>
2022-03-31 23:22:27 +0300

    Installed navigation packages

---
+++ src/providers/Navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/stack'
+
+export const Navigation = () => {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  )
+}
