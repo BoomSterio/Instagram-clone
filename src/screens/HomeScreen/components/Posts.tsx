81d6ed549635f289c9ca0b204d37c63162c1d935
Maksym <max.sharinov@gmail.com>
2022-04-10 00:10:32 +0300

    Email is userId

--- src/screens/HomeScreen/components/Posts.tsx
+++ src/screens/HomeScreen/components/Posts.tsx
@@ -1,9 +1,19 @@
 import { POSTS } from 'data'
+import { useEffect, useState } from 'react'
 import { View } from 'react-native'
 import { Divider } from 'react-native-elements/dist/divider/Divider'
+import { db } from '../../../../firebase'
 import { PostItem } from './PostItem'
 
 export const Posts = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    db.collectionGroup('posts').onSnapshot(snapshot => {
+      console.log(snapshot.docs.map(doc => doc.data()))
+    })
+  }, [])
+
   return (
     <View>
       <Divider width={1} orientation={'vertical'} />
