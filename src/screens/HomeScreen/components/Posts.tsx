b28379a336f27f8652c5775f82a63072c147e660
Maksym <max.sharinov@gmail.com>
2022-04-10 23:01:20 +0300

    usePosts hook

--- src/screens/HomeScreen/components/Posts.tsx
+++ src/screens/HomeScreen/components/Posts.tsx
@@ -1,40 +1,15 @@
-import { POSTS } from 'data'
-import { useEffect, useState } from 'react'
+import { usePosts } from 'hooks'
 import { View } from 'react-native'
 import { Divider } from 'react-native-elements/dist/divider/Divider'
-import { Post } from 'types'
-import { db } from '../../../../firebase'
 import { PostItem } from './PostItem'
 
 export const Posts = () => {
-  const [data, setData] = useState<Post[]>([])
-
-  useEffect(() => {
-    return db.collectionGroup('posts').onSnapshot((snapshot) => {
-      const posts: Post[] = snapshot.docs.map((doc) => {
-        const { userId, caption, imageUrl, username, likes, likesByUsers, comments, commentsCount, profileImageUrl } =
-          doc.data()
-        return {
-          id: doc.id,
-          userId,
-          caption,
-          imageUrl,
-          username,
-          likes,
-          likesByUsers,
-          comments,
-          commentsCount,
-          profileImageUrl,
-        }
-      })
-      setData(posts)
-    })
-  }, [])
+  const posts = usePosts()
 
   return (
     <View>
       <Divider width={1} orientation={'vertical'} />
-      {data.map((post) => (
+      {posts.map((post) => (
         <PostItem key={post.id} post={post} />
       ))}
     </View>
